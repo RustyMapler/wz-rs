be710bef8d2182017f6670041d8819f9d0d02cfa
Sebastian <SebastianDang@users.noreply.github.com>
2024-09-23 10:54:56 -0700

    refactor: Remove fileset

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod properties;
 pub mod util;
 pub mod wz_directory;
 pub mod wz_file;
-pub mod wz_file_set;
 pub mod wz_object;
 pub mod wz_reader;
 
@@ -12,6 +11,5 @@ pub use properties::*;
 pub use util::*;
 pub use wz_directory::*;
 pub use wz_file::*;
-pub use wz_file_set::*;
 pub use wz_object::*;
 pub use wz_reader::*;
