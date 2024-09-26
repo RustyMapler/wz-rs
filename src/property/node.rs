8844f6a5f670f5578a8dc58c2fa2ed894980ada6
Sebastian <SebastianDang@users.noreply.github.com>
2024-09-25 18:07:56 -0700

    Clean up a bit

--- src/property/node.rs
+++ src/property/node.rs
@@ -1,7 +1,5 @@
 use std::{
-    collections::HashMap,
-    error::Error,
-    sync::{Arc, Weak},
+    collections::HashMap, error::Error, io::ErrorKind, sync::{Arc, Weak}
 };
 
 use crate::WzReader;
@@ -23,100 +21,76 @@ impl DynamicWzNode {
         value: impl Into<WzValue>,
         parent: Option<&ArcDynamicWzNode>,
     ) -> Self {
+        Self::new_with_children(name, value, parent, HashMap::new())
+    }
+
+    pub fn new_with_children(
+        name: &String,
+        value: impl Into<WzValue>,
+        parent: Option<&ArcDynamicWzNode>,
+        children: HashMap<String, Arc<DynamicWzNode>>,
+    ) -> Self {
         Self {
             name: name.clone(),
             value: value.into(),
             parent: parent.map(Arc::downgrade).unwrap_or_default(),
-            children: HashMap::new(),
+            children,
         }
     }
 }
 
-pub fn parse_property_list(
+pub fn parse_property(
     parent: Option<&ArcDynamicWzNode>,
     reader: &Arc<WzReader>,
     offset: u32,
 ) -> Result<HashMap<String, Arc<DynamicWzNode>>, Box<dyn std::error::Error>> {
-    let mut properties: HashMap<String, Arc<DynamicWzNode>> = HashMap::new();
+    let mut properties = HashMap::new();
 
     let num_entries = reader.read_wz_int()?;
     for _ in 0..num_entries {
-        // We want to continue with the next entry if this fails (Doesn't panic)
+        // Continue to the next entry without panicking if reading fails
         let name = reader.read_string_block(offset).unwrap_or_default();
-
         let property_type = reader.read_u8()?;
-        match property_type {
-            0 => {
-                properties.insert(
-                    name.clone(),
-                    Arc::new(DynamicWzNode::new(&name, WzValue::Null, parent)),
-                );
-            }
+
+        let node = match property_type {
+            0 => DynamicWzNode::new(&name, WzValue::Null, parent),
             2 | 11 => {
                 let value = reader.read_i16()?;
-                properties.insert(
-                    name.clone(),
-                    Arc::new(DynamicWzNode::new(&name, WzValue::Short(value), parent)),
-                );
+                DynamicWzNode::new(&name, WzValue::Short(value), parent)
             }
             3 | 19 => {
                 let value = reader.read_wz_int()?;
-                properties.insert(
-                    name.clone(),
-                    Arc::new(DynamicWzNode::new(&name, WzValue::Int(value), parent)),
-                );
+                DynamicWzNode::new(&name, WzValue::Int(value), parent)
             }
             20 => {
                 let value = reader.read_wz_long()?;
-                properties.insert(
-                    name.clone(),
-                    Arc::new(DynamicWzNode::new(&name, WzValue::Long(value), parent)),
-                );
+                DynamicWzNode::new(&name, WzValue::Long(value), parent)
             }
             4 => {
-                let value = reader.read_u8().and_then(|sub| {
-                    if sub == 0x80 {
-                        reader.read_f32()
-                    } else {
-                        Ok(0.0)
-                    }
-                })?;
-                properties.insert(
-                    name.clone(),
-                    Arc::new(DynamicWzNode::new(&name, WzValue::Float(value), parent)),
-                );
+                let value = match reader.read_u8()? {
+                    0x80 => reader.read_f32()?,
+                    _ => 0.0,
+                };
+                DynamicWzNode::new(&name, WzValue::Float(value), parent)
             }
             5 => {
                 let value = reader.read_f64()?;
-                properties.insert(
-                    name.clone(),
-                    Arc::new(DynamicWzNode::new(&name, WzValue::Double(value), parent)),
-                );
+                DynamicWzNode::new(&name, WzValue::Double(value), parent)
             }
             8 => {
                 let value = reader.read_string_block(offset)?;
-                properties.insert(
-                    name.clone(),
-                    Arc::new(DynamicWzNode::new(&name, WzValue::String(value), parent)),
-                );
+                DynamicWzNode::new(&name, WzValue::String(value), parent)
             }
             9 => {
-                let eob = reader.read_u32()? + reader.get_position()? as u32;
-                // let extended_property_reader = reader.clone();
-                // let extended = parse_extended_property(
-                //     root_obj,
-                //     extended_property_reader,
-                //     offset,
-                //     name.clone(),
-                // )?;
-                // properties.insert(name.clone(), extended);
-                reader.seek(eob as u64)?;
+                continue;
             }
             _ => {
-                // We want to continue with the next entry (Do not throw an error)
                 log::warn!("unsupported property type: {}, {}", name, property_type);
+                continue; // Skip unsupported types
             }
-        }
+        };
+
+        properties.insert(name, Arc::new(node));
     }
 
     Ok(properties)
