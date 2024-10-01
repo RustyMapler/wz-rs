8091c1ae03a6799ced0c0b9bf4f8bc21f0354ac2
Sebastian <SebastianDang@users.noreply.github.com>
2024-09-30 22:20:53 -0700

    Try to write .wav files

--- src/properties/types/wz_sound.rs
+++ src/properties/types/wz_sound.rs
@@ -35,39 +35,50 @@ impl fmt::Display for WzSound {
     }
 }
 
-pub fn save_sound(sound: &WzSound) -> std::io::Result<()> {
+pub fn save_sound(path: &str, sound: &WzSound) -> std::io::Result<()> {
     let sound_type = if sound.header_data.len() == 0x46 {
         "wav"
     } else {
         "mp3"
     };
 
-    let file_path = format!("assets/sound/{}.{}", sound.name, sound_type);
+    let file_path = format!("{}/{}.{}", path, sound.name, sound_type);
     let file = File::create(file_path)?;
     let mut writer = BufWriter::new(file);
 
     match sound_type {
         "wav" => {
-            const WAV_HEADER: [u8; 44] = [
-                0x52, 0x49, 0x46, 0x46, // "RIFF"
-                0, 0, 0, 0, // ChunkSize
-                0x57, 0x41, 0x56, 0x45, // "WAVE"
-                0x66, 0x6d, 0x74, 0x20, // "fmt"
-                0x10, 0, 0, 0, // chunk1Size
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // copy16char
-                0x64, 0x61, 0x74, 0x61, // "data"
-                0, 0, 0, 0, // chunk2Size
+            //https://docs.fileformat.com/audio/wav/
+            const WAV_HEADER_TEMPLATE: [u8; 44] = [
+                // RIFF chunk descriptor
+                0x52, 0x49, 0x46, 0x46, // "RIFF" in ASCII
+                0, 0, 0, 0, // Chunk size (file size - 8 bytes)
+                0x57, 0x41, 0x56, 0x45, // "WAVE" in ASCII
+                // fmt sub-chunk
+                0x66, 0x6d, 0x74, 0x20, // "fmt " in ASCII
+                0x10, 0, 0, 0, // Sub-chunk size (16 for PCM)
+                0, 0, // Audio format (1 = PCM, other values indicate compression)
+                0, 0, // Number of channels (1 for mono, 2 for stereo, etc.)
+                0, 0, 0, 0, // Sample rate (e.g., 44100 Hz), to be filled later
+                0, 0, 0,
+                0, // Byte rate (sample rate * num channels * bits per sample / 8), to be filled later
+                0, 0, // Block align (num channels * bits per sample / 8), to be filled later
+                0, 0, // Bits per sample (e.g., 16 bits), to be filled later
+                // data sub-chunk
+                0x64, 0x61, 0x74, 0x61, // "data" in ASCII
+                // Sub-chunk 2 size (data size = num samples * num channels * bits per sample / 8)
+                0, 0, 0, 0,
             ];
 
-            let u8_16_from_header = &sound.header_data[0x34..0x34 + 16];
-            let chunk1_size = (sound.buffer_size + 36).to_le_bytes();
-            let chunk2_size = sound.buffer_size.to_le_bytes();
+            let sound_format = &sound.header_data[0x34..0x34 + 16];
+            let riff_chunk_size = (sound.buffer_size + 36).to_le_bytes();
+            let data_chunk_size = sound.buffer_size.to_le_bytes();
 
-            let mut wav_header = WAV_HEADER.to_vec();
+            let mut wav_header = WAV_HEADER_TEMPLATE.to_vec();
 
-            wav_header[4..8].copy_from_slice(&chunk1_size);
-            wav_header[20..36].copy_from_slice(u8_16_from_header);
-            wav_header[40..44].copy_from_slice(&chunk2_size);
+            wav_header[4..8].copy_from_slice(&riff_chunk_size);
+            wav_header[20..36].copy_from_slice(sound_format);
+            wav_header[40..44].copy_from_slice(&data_chunk_size);
 
             writer.write_all(&wav_header)?;
             writer.write_all(&sound.buffer)?;
