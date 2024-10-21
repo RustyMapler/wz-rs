7e23e4b397e01cc63d6cda4996c218e87b70af99
Sebastian <SebastianDang@users.noreply.github.com>
2024-10-20 20:50:00 -0700

    Initial exporter

---
+++ examples/node_exporter.rs
@@ -0,0 +1,69 @@
+use serde::Serialize;
+use std::fs::File;
+use std::io;
+use std::io::Write;
+use std::sync::Arc;
+use wz::{properties::node::WzNode, resolve, WzFile, WzVersion};
+
+#[derive(Serialize)]
+struct SerializableWzNode {
+    name: String,
+    offset: usize,
+    value: String,
+    children: Vec<SerializableWzNode>,
+}
+
+impl From<&Arc<WzNode>> for SerializableWzNode {
+    fn from(node: &Arc<WzNode>) -> Self {
+        SerializableWzNode {
+            name: node.name.clone(),
+            offset: node.offset,
+            value: format!("{:?}", node.value),
+            children: node.children.values().map(|child| child.into()).collect(),
+        }
+    }
+}
+
+fn to_json(node: &Arc<WzNode>) -> String {
+    let serializable = SerializableWzNode::from(node);
+    serde_json::to_string_pretty(&serializable).unwrap()
+}
+
+fn write_json_to_file(json: &str, output_file: &str) -> io::Result<()> {
+    let mut file = File::create(output_file)?;
+    file.write_all(json.as_bytes())?;
+    Ok(())
+}
+
+fn to_node_path(map_id: &str) -> String {
+    format!("Map/Map{}/{}.img", &map_id[0..1], map_id)
+}
+
+fn main() -> io::Result<()> {
+    simple_logger::SimpleLogger::new()
+        .env()
+        .with_module_level("wz", log::LevelFilter::Error)
+        .init()
+        .unwrap();
+
+    let input_file = "assets/Map002.wz";
+    let input_map_id = "000000000";
+    let output_file = format!("assets/{}.json", input_map_id);
+
+    let mut wz_file = WzFile::new(input_file, WzVersion::GMS);
+    wz_file.open()?;
+
+    let root = wz_file.parse_root_directory()?;
+
+    let path = to_node_path(input_map_id);
+
+    if let Ok(node) = resolve(&root, &path) {
+        let json = to_json(&node);
+        write_json_to_file(&json, &output_file)?;
+        log::info!("JSON data written to {}", output_file);
+    } else {
+        log::error!("Node not found for path: {}", path);
+    }
+
+    Ok(())
+}
